fd3fd489f94d22ba86dd4f7bb568ef1a7dc85841
Joshua Manalo <joshuavincemanalo@gmail.com>
2020-04-02 01:57:43 -0500

    Updates

---
+++ precache-manifest.8e386008cfed90306b9bf71aa094e0c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282acaf37d80d74cddda26057be214b8",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "e29ff0da2ba1d78b3d87",
+    "url": "/todo-list/static/css/main.62824493.chunk.css"
+  },
+  {
+    "revision": "21ccdf189446903875cf",
+    "url": "/todo-list/static/js/2.87931fe3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list/static/js/2.87931fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29ff0da2ba1d78b3d87",
+    "url": "/todo-list/static/js/main.7db8aafe.chunk.js"
+  },
+  {
+    "revision": "da50543231f9ec866448",
+    "url": "/todo-list/static/js/runtime-main.e65c8737.js"
+  }
+]);
